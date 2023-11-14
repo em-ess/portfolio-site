471d084907bc92b8227d5a6acf789f31cd6e7d47
Michelle Song <michssong@gmail.com>
2023-11-14 13:10:41 -0600

    Moved project card to new component. Added framer motion to project cards.

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
-import Image from "next/image";
+import Project from "./project";
 
 export default function Projects() {
   return (
-    <section>
+    <section id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
@@ -14,44 +16,6 @@ export default function Projects() {
           </React.Fragment>
         ))}
       </div>
-    </section>
-  );
-}
-type ProjectProps = (typeof projectsData)[number];
-
-function Project({ title, description, tags, imageUrl }: ProjectProps) {
-  return (
-    <section className="group bg-gray-200 max-w-[42rem] mb-3 border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8">
-      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-        <h3 className="text-2xl font-semibold">{title}</h3>
-        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-        <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-          {tags.map((tag, index) => (
-            <li
-              className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-              key={index}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Image
-        src={imageUrl}
-        alt="Project"
-        quality={95}
-        className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition 
-        group-hover:scale-[1.04]
-        group-hover:-translate-x-3
-        group-hover:translate-y-3
-        group-hover:-rotate-2
-
-        group-even:group-hover:translate-x-3
-        group-even:group-hover:translate-y-3
-        group-even:group-hover:rotate-2 
-        
-        group-even:right-[initial] group-even:-left-40"
-      />
     </section>
   );
 }
